a06e6b8e7599be290b691653dee74ed25954e3f3
Viktor Arsanov <viktor.arsanov@hotmail.com>
2026-02-11 20:06:36 +0300

    Added  migration

---
+++ Migrations/20260211170630_ResourceAndScopeAttributes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OtlpServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResourceAndScopeAttributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResourceAttributes",
+                table: "TraceEntries",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ScopeAttributes",
+                table: "TraceEntries",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResourceAttributes",
+                table: "TraceEntries");
+
+            migrationBuilder.DropColumn(
+                name: "ScopeAttributes",
+                table: "TraceEntries");
+        }
+    }
+}
